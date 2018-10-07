e7f9c00d5a9f71079d182236668a4217a7c0c32f
omer cohen <omer260792@gmail.com>
2018-10-07 17:45:58 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Omer Cohen on 10/7/18.
+//  Copyright © 2018 Omer Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
